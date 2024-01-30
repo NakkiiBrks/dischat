7a299060b599d3607a4d3dc5f1f6f35538736e60
NakkiiBrks <nakkii800br@gmail.com>
2024-01-30 02:17:13 -0300

    Dark & Light mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,12 @@
 import { UserButton } from '@clerk/nextjs';
+import { ModeToggle } from '@/components/mode-toggle';
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl='/'></UserButton>
+      <UserButton
+        afterSignOutUrl='/'
+      />
+      <ModeToggle />
     </div>
   )
 }
